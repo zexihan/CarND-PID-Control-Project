bbe2f20fc0d05e358b010a7b65f1254c15df3ee0
zexihan <zexihan@outlook.com>
2020-05-06 16:07:56 -0700

    complete the code

--- src/PID.h
+++ src/PID.h
@@ -2,49 +2,50 @@
 #define PID_H
 
 class PID {
- public:
-  /**
-   * Constructor
-   */
-  PID();
+public:
+    /**
+     * PID Errors
+     */
+    double p_error;
+    double i_error;
+    double d_error;
 
-  /**
-   * Destructor.
-   */
-  virtual ~PID();
 
-  /**
-   * Initialize PID.
-   * @param (Kp_, Ki_, Kd_) The initial PID coefficients
-   */
-  void Init(double Kp_, double Ki_, double Kd_);
+    /**
+     * PID Coefficients
+     */
+    double Kp;
+    double Ki;
+    double Kd;
+    
+    /**
+     * Constructor
+     */
+    PID();
 
-  /**
-   * Update the PID error variables given cross track error.
-   * @param cte The current cross track error
-   */
-  void UpdateError(double cte);
+    /**
+     * Destructor.
+     */
+    virtual ~PID();
 
-  /**
-   * Calculate the total PID error.
-   * @output The total PID error
-   */
-  double TotalError();
+    /**
+     * Initialize PID.
+     * @param (Kp_, Ki_, Kd_) The initial PID coefficients
+     */
+    void Init(double Kp_, double Ki_, double Kd_);
 
- private:
-  /**
-   * PID Errors
-   */
-  double p_error;
-  double i_error;
-  double d_error;
+    /**
+     * Update the PID error variables given cross track error.
+     * @param cte The current cross track error
+     */
+    void UpdateError(double cte);
 
-  /**
-   * PID Coefficients
-   */ 
-  double Kp;
-  double Ki;
-  double Kd;
+    /**
+     * Calculate the total PID error.
+     * @output The total PID error
+     */
+    double TotalError();
+    
 };
 
 #endif  // PID_H
